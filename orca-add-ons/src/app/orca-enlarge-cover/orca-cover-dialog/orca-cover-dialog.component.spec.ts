f6c5646d01d61819f13fe3d7812e301f74b07cd5
Tamara Marnell <tmarnell@orbiscascade.org>
2026-06-05 15:50:24 -0700

    Add Orca prefix to CoverDialogComponent spec

--- orca-add-ons/src/app/orca-enlarge-cover/orca-cover-dialog/orca-cover-dialog.component.spec.ts
+++ orca-add-ons/src/app/orca-enlarge-cover/orca-cover-dialog/orca-cover-dialog.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CoverDialogComponent } from './cover-dialog.component';
+import { OrcaCoverDialogComponent } from './orca-cover-dialog.component';
 
-describe('CoverDialogComponent', () => {
-  let component: CoverDialogComponent;
-  let fixture: ComponentFixture<CoverDialogComponent>;
+describe('OrcaCoverDialogComponent', () => {
+  let component: OrcaCoverDialogComponent;
+  let fixture: ComponentFixture<OrcaCoverDialogComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CoverDialogComponent]
+      imports: [OrcaCoverDialogComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(CoverDialogComponent);
+    fixture = TestBed.createComponent(OrcaCoverDialogComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
