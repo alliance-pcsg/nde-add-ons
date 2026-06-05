dbe468edae5c3a480c6cfffe4853538a806ce588
Tamara Marnell <tmarnell@orbiscascade.org>
2026-06-05 15:53:50 -0700

    Add Orca prefix to EnlargeCoverComponent spec

--- orca-add-ons/src/app/orca-enlarge-cover/orca-enlarge-cover.component.spec.ts
+++ orca-add-ons/src/app/orca-enlarge-cover/orca-enlarge-cover.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { OrcaEnlargeCoverComponent } from './orca-enlarge-cover.component';
 
-describe('EnlargeCoverComponent', () => {
+describe('OrcaEnlargeCoverComponent', () => {
   let component: OrcaEnlargeCoverComponent;
   let fixture: ComponentFixture<OrcaEnlargeCoverComponent>;
 
